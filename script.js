b75758bb42dba3449fc1736857f251479b082017
gl-el <118758307+gl-el@users.noreply.github.com>
2022-12-28 07:56:18 +0300

    functions for basic math operations

---
+++ script.js
@@ -0,0 +1,21 @@
+//базовые математические операции
+//сложение
+function add(a, b) {
+    return a + b;
+}
+//вычитание
+function subtract(a, b) {
+    return a - b;
+}
+//умножение
+function multiply(a, b) {
+    return a*b;
+}
+//деление
+function divide(a,b) {
+    return a/b;
+}
+console.log(add(3,6));
+console.log(subtract(2,3));
+console.log(multiply(-2,4));
+console.log(divide(-6,4));
