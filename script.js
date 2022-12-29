eb000e500063130e4d8e41beb8cc05e09794c4a6
gl-el <118758307+gl-el@users.noreply.github.com>
2022-12-29 06:27:56 +0300

    added base classes

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
-//фнукция вызывающая выбранную операцию и подставляющая в нее значения
+//функция вызывающая выбранную операцию и подставляющая в нее значения
 function operate(op, a, b) {
     switch (op) {
         case "add":
@@ -28,7 +28,3 @@ function operate(op, a, b) {
             return divide(a, b);
     }
 }
-console.log(operate("add",2,3));
-console.log(operate("sbtrct",6,3));
-console.log(operate("mltpl",-5,2));
-console.log(operate("dvd",6,2));
