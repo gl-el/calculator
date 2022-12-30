3467cf185ca58696328438745a0995106ec2d8ae
gl-el <118758307+gl-el@users.noreply.github.com>
2022-12-30 07:57:01 +0300

    Working with buttons

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //базовые математические операции
 //сложение
+let firstNumber, secondNumber;//переменные для математических операций
 function add(a, b) {
     return a + b;
 }
@@ -28,3 +29,55 @@ function operate(op, a, b) {
             return divide(a, b);
     }
 }
+let digit; //переменная для хранения цифр с клавиатуры
+let number = "";//переменная для хранения введенного числа
+//получаем значения цифр с кнопок
+const btnsNum = document.querySelectorAll("button.number");
+btnsNum.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        digit = e.target.value;//получаем значение с кнопки
+        number += digit.toString();//добавляем цифру к числу
+        displayCurr();
+    });
+});
+//выводим числа на экран в основное поле
+const displayCurrent = document.querySelector(".display-current");
+function displayCurr() {
+    displayCurrent.textContent = number;
+}
+//вывод чисел на экран в дополнительное поле
+const displayLast = document.querySelector(".display-last");
+function displayLst() {
+    displayLast.textContent = `${firstNumber} ${operationValue}`;
+}
+//получаем значения операций с кнопок
+let operation = ""//переменная для хранения операции с кнопок
+let operationValue = ""//переменная для хранения значений кнопок с операциями
+let operationCount = 0;//счетчик количества нажатий на операции
+const btnsOp = document.querySelectorAll("button.operations");
+btnsOp.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        operation = e.target.id;
+        operationValue = e.target.value;
+        operationCount++;
+        firstNumber = number;
+        number = "";
+        displayLst();
+        displayCurr();
+    });
+});
+//функция очистки значений
+function clearDisplay() {
+    firstNumber = "";
+    secondNumber = "";
+    number = ""
+    operationValue = ""
+    operationCount = 0;
+    displayCurr();
+    displayLst();
+}
+const btnClear = document.getElementById("clear");
+btnClear.addEventListener('click', () => {
+    clearDisplay();
+})
+//
